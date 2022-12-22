1e32afeb2ae6a3bc7989d08ee7490850f6678ce9
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-22 19:42:16 +0100

    test heroku deploy

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -8,6 +10,12 @@ app.use(express.static(path.resolve(__dirname, "./client/build")));
 //   res.sendFile(path.join(__dirname, "build", "index.html"))
 // })
 
-// app.get("/*", (req, res)=>{
-//   res.sendFile(path.resolve('client', "build", "index.html"))
-// })
+app.get("/*", (_, res) => {
+	res.sendFile(path.resolve("client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+});
